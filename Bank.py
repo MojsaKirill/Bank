80df4ab087231b862d4a8a8cd286916134cfa6c3
MojsaKirill <lirikmoysa@gmail.com>
2019-03-15 13:13:18 +0300

    add time

--- Bank.py
+++ Bank.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import tensorflow as tf
+import time
+
+start_time = time.time()
 
 dataset = pd.read_csv("csv/boolean_bank.csv", sep=',').to_numpy()
 
@@ -12,9 +15,11 @@ model.add(tf.keras.layers.Dense(1, activation='sigmoid', kernel_initializer='ran
 
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
-model.fit(X, Y, epochs=1000, batch_size=32)
+model.fit(X, Y, epochs=20, batch_size=32)
 
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
 model.save('bank_model.h5')
+
+print(time.time()-start_time)
