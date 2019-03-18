3255d98c8abf29376b3f21c76de7ddf014dc1f75
MojsaKirill <lirikmoysa@gmail.com>
2019-03-18 14:26:56 +0300

    add balans columns

--- BankLSTM.py
+++ BankLSTM.py
@@ -14,14 +14,14 @@ X, Y = dataset[:, 0:22], dataset[:, 22]
 X = X.reshape((X.shape[0], 1, X.shape[1]))
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.LSTM(44, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(tf.keras.layers.LSTM(1024, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
 model.add(tf.keras.layers.LSTM(22, return_sequences=True))
 model.add(tf.keras.layers.LSTM(11, return_sequences=True))
 model.add(tf.keras.layers.LSTM(5))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid', kernel_initializer='random_uniform'))
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
-model.fit(X, Y, epochs=1000, batch_size=32)
+model.fit(X, Y, epochs=1000, batch_size=2048)
 
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
